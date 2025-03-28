29a609b5a6790b9dbbb7ac91cf41cdc27806643c
jniyitegek <j.niyitegek@alustudent.com>
2025-03-28 11:27:21 +0200

    The script that connect users local server and the scripts of this project

---
+++ database/database.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+def connect_db():
+    return mysql.connector.connect(
+        host="localhost",  
+        user='root',
+        password='bruest2500',
+        database="career_pathfinder"     
+    )
+
